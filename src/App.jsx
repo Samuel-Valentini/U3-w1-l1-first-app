7c7084d77451f21b5b27fb0abf2de9fbe0cf1064
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-02 16:41:53 +0100

    lighter structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import H4Component from "./components/H4Component";
 import ImageComponent from "./components/ImageComponent";
 import H5Component from "./components/H5Component";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <H4Component content="Questa è un'immagine casuale" />
